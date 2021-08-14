fce20067df67ed17b87c8ce2881d6ce272aa6d3c
sharif764 <ss1884088@gmail.com>
2021-08-14 23:57:45 +0600

    jJ

--- js/login.js
+++ js/login.js
@@ -5,7 +5,12 @@ document.getElementById('login-submit').addEventListener('click',function(){
    //userPassword 
    const passwordField = document.getElementById('user-password');
    const userPassword = passwordField.value;
+   // check email and password and goto another page 
    if(userEmail == 'sontan@baap.com' && userPassword == 'secret'){
        console.log('valid user');
+       window.location.href = 'banking.html';
+   }
+   else{
+       alert('please enter a valid password and gmail id');
    }
 })
